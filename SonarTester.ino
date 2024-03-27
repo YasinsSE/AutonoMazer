3f366e84bb2dce35ffc10712997d58122cd98fc7
Yasin YILDIRIM <123670204+YasinsSE@users.noreply.github.com>
2024-03-27 11:23:19 +0300

    Update SonarTester.ino

--- SonarTester.ino
+++ SonarTester.ino
@@ -1,13 +1,11 @@
+// This program tests 3 ultrasonic sensors to evaluate their measurement accuracy and determine the distances they measure.
 // This uses Serial Monitor to display Range Finder distance readings
-// Include NewPing Library
 #include "NewPing.h"
-// Hook up HC-SR04 with Trig to Arduino Pin 9, Echo to Arduino pin 10
-//#define TRIGGER_PIN 9
-//#define ECHO_PIN 10
-#define leftTrig 0
-#define leftEcho 1
-#define frontTrig 2
-#define frontEcho 3
+// HC-SR04
+#define leftTrig 2
+#define leftEcho 3
+#define frontTrig 8
+#define frontEcho 11
 #define rightTrig 4
 #define rightEcho 5
 // Maximum distance we want to ping for (in centimeters).
@@ -34,36 +32,17 @@ void loop()
   
   // Send results to Serial Monitor
   Serial.print("Distance to front = ");
-  if (frontDistance >= 400 || frontDistance <= 2) 
-  {
-    Serial.println("Out of range");
-  }
-  else 
-  {
-    Serial.print(frontDistance);
-    Serial.println(" cm -front-\n");
-  }
+  Serial.print(frontDistance);
+  Serial.println(" cm -front-");
 
   Serial.print("Distance to left = ");
-  if (leftDistance >= 400 || leftDistance <= 2) 
-  {
-    Serial.println("Out of range");
-  }
-  else 
-  {
-    Serial.print(leftDistance);
-    Serial.println(" cm -left-\n");
-  }
-
+  Serial.print(leftDistance);
+  Serial.println(" cm -left-");
+  
   Serial.print("Distance to right = ");
-  if (rightDistance >= 400 || rightDistance <= 2) 
-  {
-    Serial.println("Out of range");
-  }
-  else 
-  {
-    Serial.print(rightDistance);
-    Serial.println(" cm -right-\n");
-  }
-  delay(500);
+  Serial.print(rightDistance);
+  Serial.println(" cm -right-");
+
+  Serial.println(" ----------------------------------------- ");
+  delay(6000);
 }
